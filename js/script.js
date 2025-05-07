1349186df66cd85ce71e8f6ef80295dca9aca524
jishy <jishe@duck.com>
2025-05-07 19:26:26 -0400

    added light/dark theme toggle

--- js/script.js
+++ js/script.js
@@ -16,6 +16,30 @@ function copyToClipboard(text) {
 		});
 }
 
+// Theme toggling
+function initThemeToggle() {
+	const themeToggle = document.getElementById("theme-toggle");
+
+	// Check cookie setting
+	const savedTheme = localStorage.getItem("theme");
+
+	// Apply saved theme if exists
+	if (savedTheme === "light") {
+		document.body.classList.add("light-theme");
+	}
+
+	// Add click event to toggle theme
+	themeToggle.addEventListener("click", () => {
+		document.body.classList.toggle("light-theme");
+
+		// Save current theme preference
+		const currentTheme = document.body.classList.contains("light-theme")
+			? "light"
+			: "dark";
+		localStorage.setItem("theme", currentTheme);
+	});
+}
+
 // Animation for name changing with obfuscation
 const names = ["jishy", "jishe"];
 const chars = "abcdefghijklmnopqrstuvwxyz";
@@ -107,4 +131,5 @@ function animateName() {
 
 window.addEventListener("DOMContentLoaded", () => {
 	animateName();
+	initThemeToggle();
 });
